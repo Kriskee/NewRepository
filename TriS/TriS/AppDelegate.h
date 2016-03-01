9237c6dc2e26d348791e7ebc877d12a2263c1bdf
lanou3g <lanou3g@liuchaokai.local>
2016-03-01 10:35:37 +0800

    3月1日，刘超凯：第一次上传工程

---
+++ TriS/TriS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TriS
+//
+//  Created by lanou3g on 16/3/1.
+//  Copyright © 2016年 3Singles. All rights reserved.
+//
+/*
+ * 日志:2016.3.1
+ * 3S, TriS 
+ * 3 的英文为 Tri
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TriS/TriS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TriS
+//
+//  Created by lanou3g on 16/3/1.
+//  Copyright © 2016年 3Singles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
